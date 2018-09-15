535a1c9e78fe90def051afb2c5ef73410ea780f6
danieljonesdmj <danieljonesdmj@gmail.com>
2018-09-15 17:36:40 +0100

    Add initial tests

---
+++ spec/DeckSpec.js
@@ -0,0 +1,8 @@
+describe('Deck', function(){
+  describe('initial tests', function(){
+    it('checks that class is defined', function(){
+      deck = new Deck();
+      expect(deck).toBeDefined();
+    })
+  })
+})
