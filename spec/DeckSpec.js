fa0f38754cde25ba89d22c2673280411173098cd
danieljonesdmj <danieljonesdmj@gmail.com>
2018-09-16 08:35:23 +0100

    Refactor tests

--- spec/DeckSpec.js
+++ spec/DeckSpec.js
@@ -1,18 +1,19 @@
 describe('Deck', function(){
+  beforeEach(function() {
+    deck = new Deck();
+  })
+
   describe('initial tests', function(){
     it('checks that class is defined', function(){
-      deck = new Deck();
       expect(deck).toBeDefined();
     })
     
     it('checks that class instantiates with an empty object', function(){
-      deck = new Deck();
       expect(deck.cards).toEqual([]);
     })
   })
   describe('card rank array', function(){
     it('holds all card ranks', function(){
-      deck = new Deck();
       expect(deck.cardRanks).toContain("three");
     })
   })
