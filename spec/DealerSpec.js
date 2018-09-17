f274e548c801fec2a299636e96fdee4703b2b11a
danieljonesdmj <danieljonesdmj@gmail.com>
2018-09-17 17:42:17 +0100

    Create dealer class - set initial test

---
+++ spec/DealerSpec.js
@@ -0,0 +1,8 @@
+describe('Dealer', function () {
+    describe('initial tests', function () {
+        it('checks that class is defined', function () {
+            dealer = new Dealer();
+            expect(dealer).toBeDefined();
+        })
+    })
+})
