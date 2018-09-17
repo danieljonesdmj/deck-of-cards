a61cb5c9a45d5b26afcfc5a41f1d42ad86792e8e
danieljonesdmj <danieljonesdmj@gmail.com>
2018-09-17 17:11:14 +0100

    Refactor - formatting

--- spec/DeckSpec.js
+++ spec/DeckSpec.js
@@ -1,44 +1,44 @@
-describe('Deck', function(){
-  beforeEach(function() {
+describe('Deck', function () {
+  beforeEach(function () {
     deck = new Deck();
   })
 
-  describe('initial tests', function(){
-    it('checks that class is defined', function(){
+  describe('initial tests', function () {
+    it('checks that class is defined', function () {
       expect(deck).toBeDefined();
     })
-    
-    it('checks that class instantiates with an empty object', function(){
+
+    it('checks that class instantiates with an empty object', function () {
       expect(deck.cards).toEqual([]);
     })
   })
 
-  describe('card rank array', function(){
-    it('holds all card ranks', function(){
+  describe('card rank array', function () {
+    it('holds all card ranks', function () {
       expect(deck.cardRanks).toContain("three");
     })
   })
 
-  describe('card suit array', function(){
-    it('holds all card suits', function(){
+  describe('card suit array', function () {
+    it('holds all card suits', function () {
       expect(deck.cardSuits).toContain("clubs");
     })
   })
 
-  describe('#buildDeck', function(){
-    it('builds a full deck of cards', function(){
-      deck.buildDeck();   
+  describe('#buildDeck', function () {
+    it('builds a full deck of cards', function () {
+      deck.buildDeck();
       expect(deck.cards.length).toEqual(52);
     })
 
-    it('built deck includes cards', function(){
+    it('built deck includes cards', function () {
       deck.buildDeck();
-      expect(deck.cards).toContain({suit: "hearts", rank: "king"});
+      expect(deck.cards).toContain({ suit: "hearts", rank: "king" });
     })
-    
-    it('built deck should not have invalid cards', function(){
+
+    it('built deck should not have invalid cards', function () {
       deck.buildDeck();
-      expect(deck.cards).not.toContain({suit: "bells", rank: "eleven"});      
+      expect(deck.cards).not.toContain({ suit: "bells", rank: "eleven" });
     })
   })
 })
