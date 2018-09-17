7cc63604a593798d95132d68fa0181f6d30bcc64
danieljonesdmj <danieljonesdmj@gmail.com>
2018-09-17 17:12:12 +0100

    Create shuffle function to shuffle ordered cards

--- src/Deck.js
+++ src/Deck.js
@@ -1,13 +1,14 @@
-function Deck(){
+function Deck(shuffle = new Shuffle()) {
   this.cards = [];
   this.cardRanks = ["ace", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king"]
   this.cardSuits = ["hearts", "clubs", "spades", "diamonds"]
+  this.shuffle = shuffle
 }
 
-Deck.prototype.buildDeck = function() {
+Deck.prototype.buildDeck = function () {
   this.cardSuits.forEach(function (suit) {
     this.cardRanks.forEach(function (rank) {
-      this.cards.push({suit, rank});
+      this.cards.push({ suit, rank });
     }, this)
   }, this)
 }
--- src/Shuffle.js
+++ src/Shuffle.js
@@ -1,3 +1,21 @@
-function Shuffle(){
+function Shuffle() {
     this.shuffledCards = [];
+}
+
+Shuffle.prototype.shuffleDeck = function (orderedCards) {
+    var currentIndex = orderedCards.length, temporaryValue, randomIndex;
+    // While the index is not 0
+    while (currentIndex !== 0) {
+
+        // Pick a random number
+        randomIndex = Math.floor(Math.random() * currentIndex--);
+        // Get the element from the array at the current index
+        temporaryValue = orderedCards[currentIndex];
+        // Replaces the element at the current index with the element at the random index
+        orderedCards[currentIndex] = orderedCards[randomIndex];
+        // Gets an element at the random index and sets it to the temporary value
+        orderedCards[randomIndex] = temporaryValue;
+    }
+
+    this.shuffledCards.push(orderedCards);
 }
