2cec9e7d8f0f1cdabcd27a54f12c60fad7b58b2d
danieljonesdmj <danieljonesdmj@gmail.com>
2018-09-17 17:36:12 +0100

    Refactor tests

--- spec/ShuffleSpec.js
+++ spec/ShuffleSpec.js
@@ -1,20 +1,22 @@
 describe('Shuffle', function () {
+    beforeEach(function () {
+        shuffle = new Shuffle();
+    })
+
     describe('initial tests', function () {
         it('checks that class is defined', function () {
-            shuffle = new Shuffle();
             expect(shuffle).toBeDefined();
         })
 
         it('checks that class instantiates with an empty object', function () {
-            shuffle = new Shuffle();
             expect(shuffle.shuffledCards).toEqual([]);
         })
 
         describe('#shuffleDeck', function () {
             it('shuffles the ordered cards', function () {
-                shuffle = new Shuffle();
-                shuffle.shuffleDeck([{ suit: "hearts", rank: "ace" }, { suit: "hearts", rank: "two" }, { suit: "hearts", rank: "three" }, { suit: "hearts", rank: "four" }, { suit: "hearts", rank: "five" }])
-                expect(shuffle.shuffledCards).not.toEqual([{ suit: "hearts", rank: "ace" }, { suit: "hearts", rank: "two" }, { suit: "hearts", rank: "three" }, { suit: "hearts", rank: "four" }, { suit: "hearts", rank: "five" }])
+                var testArray = [{ suit: "hearts", rank: "ace" }, { suit: "hearts", rank: "two" }, { suit: "hearts", rank: "three" }, { suit: "hearts", rank: "four" }, { suit: "hearts", rank: "five" }]
+                shuffle.shuffleDeck(testArray)
+                expect(shuffle.shuffledCards).not.toEqual(testArray)
             })
         })
     })
